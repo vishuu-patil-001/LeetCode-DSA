6ddb2435adedcb957a7949c7644268edd27fad37
Vishwjit Upase <vishwjitupase2807@gmail.com>
2026-03-20 23:29:28 +0530

    Time: 3 ms (32.35%) | Memory: 53.7 MB (86.84%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -0,0 +1,15 @@
+var longestCommonPrefix = function(strs) {
+    if (strs.length === 0) return "";
+
+    for (let i = 0; i < strs[0].length; i++) {
+        let char = strs[0][i];
+
+        for (let j = 1; j < strs.length; j++) {
+            if (i >= strs[j].length || strs[j][i] !== char) {
+                return strs[0].substring(0, i);
+            }
+        }
+    }
+
+    return strs[0];
+};
