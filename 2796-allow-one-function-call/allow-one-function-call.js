2ee906c88a982ef140eb104290b33a740e824a91
Vishwjit Upase <vishwjitupase2807@gmail.com>
2026-03-23 21:46:30 +0530

    Time: 43 ms (63.07%) | Memory: 52.6 MB (95.40%) - LeetSync

---
+++ 2796-allow-one-function-call/allow-one-function-call.js
@@ -0,0 +1,11 @@
+var once = function(fn) {
+    let called = false;
+
+    return function(...args) {
+        if (!called) {
+            called = true;
+            return fn(...args);
+        }
+        return undefined;
+    };
+};
